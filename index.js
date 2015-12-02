b92cc9b12cf378625d34da7187caf50b24754a0b
Matthew Andrews <matt@mattandre.ws>
2015-12-02 18:24:43 +0000

    Stop using test-s3o

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ var authS3O = function(req, res, next) {
 
 		// Send the user to s3o to authenticate
 		var protocol = (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "https") ? "https" : req.protocol;
-		var s3o_url = "http://test-s3o.ft.com/v2/authenticate?post=true&host=" + encodeURIComponent(req.hostname) + "&redirect=" + encodeURIComponent(protocol + "://" + req.headers.host + req.originalUrl);
+		var s3o_url = "https://s3o.ft.com/v2/authenticate?post=true&host=" + encodeURIComponent(req.hostname) + "&redirect=" + encodeURIComponent(protocol + "://" + req.headers.host + req.originalUrl);
 		debug("S3O: No token/s3o_username found. Redirecting to " + s3o_url);
 
 		// Don't cache any redirection responses.
