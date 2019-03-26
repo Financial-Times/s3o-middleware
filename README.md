7c7152110eca3cab3899daef101454c6fa68abdf
Charlie Briggs <charliebriggs@gmail.com>
2019-03-26 15:20:39 +0000

    Upgrade s3o-middleware-utils to 2.0.0 (#53)
    This removes express cookie utilities from s3o-middleware-utils, and exports a factory function from authenticate which takes a sync public key getter and returns authenticateToken and validate methods. This was to allow `s3o-lambda` to consume the same library without immediately polling for public key changes (https://github.com/Financial-Times/s3o-middleware-utils/pull/8).
    
    This change adds the cookie utils to this library and adjusts the calls to the middleware utils to keep the existing API intact. I'd have liked to expose this middleware with a factory function as an entry point as well:
    
    * Prevents immediate eager calls to the public key endpoint
    * Makes it easier to tests APIs using this library without having to disable/stub s3o or do a conditional require
    
    however I don't personally have the appetite to manage that upgrade process.
    
    Note: s3o-middleware-utils now requires node.js >= 8. This is due to an ft-poller version update including native async/await calls. This should result in this being a major release. node.js 6 supports ends at the end of next month (April 2019) so I believe this is a reasonable breaking change at this point.
    
    This would not require consumers to update immediately.

--- README.md
+++ README.md
@@ -1,42 +1,52 @@
 # S3O-middleware
+
 Middleware to handle authenticating with [S3O](http://s3o.ft.com/docs)
 
 ## Parsing cookies
+
 This middleware can parse standard cookies via the [cookie](http://npmjs.com/package/cookie) package. If wanting to use signed cookies or json cookies, please use the [cookie-parser](https://www.npmjs.com/package/cookie-parser) middleware before using the S3O middleware.
 
-# Finding the username of the logged in user
+## Finding the username of the logged in user
+
 The username can be found in the request cookie, under `req.cookies.s3o_username`.
 
-# Setting the ttl of the cookie for an authenticated request
+## Setting the ttl of the cookie for an authenticated request
+
 Defaults to fifteen minutes. Use Express' `app.set` function before sending users to authenticate:
 `app.set('s3o-cookie-ttl', 86400000); // one day (in ms)`
 
 ## Usage example for Express
+
 If many routes require auth:
+
 ```js
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // Add routes here which don't require auth
-var authS3O = require('@financial-times/s3o-middleware');
+const authS3O = require('@financial-times/s3o-middleware');
 app.use(authS3O);
 // Add routes here which require auth
 ```
+
 If only paths within a given directory require auth:
+
 ```js
-var express = require('express');
-var app = express();
-var router = express.Router();
-var authS3O = require('@financial-times/s3o-middleware');
+const express = require('express');
+const app = express();
+const router = express.Router();
+const authS3O = require('@financial-times/s3o-middleware');
 router.use(authS3O);
 app.use('/admin', router);
 ```
+
 If specific paths require auth:
+
 ```js
-var express = require('express');
-var app = express();
-var router = express.Router();
-var authS3O = require('@financial-times/s3o-middleware');
+const express = require('express');
+const app = express();
+const router = express.Router();
+const authS3O = require('@financial-times/s3o-middleware');
 
 app.get('/', authS3O, router);
 app.post('/', authS3O);
@@ -49,10 +59,10 @@ cookies are not present or are invalid, the `authS3ONoRedirect`
 middleware will respond with a simple `403: Forbidden` response:
 
 ```js
-var express = require('express');
-var app = express();
-var router = express.Router();
-var authS3ONoRedirect = require('@financial-times/s3o-middleware').authS3ONoRedirect;
+const express = require('express');
+const app = express();
+const router = express.Router();
+const { authS3ONoRedirect } = require('@financial-times/s3o-middleware');
 
 app.get('/some-authenticated-api', authS3ONoRedirect, router);
 ```
@@ -62,19 +72,20 @@ redirect to the http version. You can override this by adding express middleware
 to force the protocol of the redirect url.
 
 ```js
-app.use('/', function (req, res, next) {
-    req.headers['x-forwarded-proto'] = 'https';
-    next();
+app.use('/', function(req, res, next) {
+	req.headers['x-forwarded-proto'] = 'https';
+	next();
 });
 ```
 
 ### Upgrade to s3o version 4
 
 Set `x-s3o-version` header to 'v4' and optionally pass a system-code header `x-s3o-systemcode`
+
 ```js
-app.use('/', function (req, res, next) {
-    req.headers['x-s3o-version'] = 'v4';
-    req.headers['x-s3o-systemcode'] = 'your-system-code';
-    next();
+app.use('/', function(req, res, next) {
+	req.headers['x-s3o-version'] = 'v4';
+	req.headers['x-s3o-systemcode'] = 'your-system-code';
+	next();
 });
 ```
--- test/s3o-middleware.test.js
+++ test/s3o-middleware.test.js
@@ -1,10 +1,9 @@
 /**
  * Basic spec for s3o-middleware
  *
- * N.b., this approach doesn't exercise authenticateToken at all.
- * @TODO Write a test spec for authenticateToken.
  */
 
+const { cookies } = require('@financial-times/s3o-middleware-utils');
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
@@ -13,12 +12,12 @@ const proxyquire = require('proxyquire');
 chai.should();
 chai.use(sinonChai);
 
-const validatorStub = sinon.stub();
-
-const nextStub = sinon.stub();
-nextStub.returns('next returned');
-
 describe('s3o-middleware', () => {
+	let sandbox;
+	let pollerStub;
+	let authenticateTokenStub;
+	let validatorStub;
+	let nextStub;
 	let reqFixture;
 	let resFixture;
 	let s3o;
@@ -44,14 +43,20 @@ describe('s3o-middleware', () => {
 	];
 
 	beforeEach(() => {
-		validatorStub.resetHistory();
-		nextStub.resetHistory();
+		sandbox = sinon.createSandbox();
+
+		pollerStub = sandbox.stub();
+		pollerStub.returns(Promise.resolve())
+		authenticateTokenStub = sandbox.stub();
+		validatorStub = sandbox.stub();
+		nextStub = sandbox.stub();
+		nextStub.returns('next returned');
 
 		// Reset overridden fixtures
 		reqFixture = {
 			cookies: {
-				s3o_username: 'test',
-				s3o_token: 'test-test-123',
+				[cookies.USERNAME]: 'test',
+				[cookies.TOKEN]: 'test-test-123',
 			},
 			hostname: 'localhost',
 			headers: {
@@ -60,30 +65,60 @@ describe('s3o-middleware', () => {
 		};
 
 		resFixture = {
-			clearCookie: sinon.spy(),
-			status: sinon.spy(),
-			send: sinon.spy(),
-			header: sinon.spy(),
+			clearCookie: sandbox.spy(),
+			status: sandbox.spy(),
+			send: sandbox.spy(),
+			header: sandbox.spy(),
 			locals: {},
 			app: {
 				get: () => 9999,
 			},
-			cookie: sinon.spy(),
-			redirect: sinon.stub(),
+			cookie: sandbox.spy(),
+			redirect: sandbox.stub(),
 		};
 
 		s3o = proxyquire('../', {
-			'@financial-times/s3o-middleware-utils/authenticate': {
-				authenticateToken: validatorStub,
+			'@financial-times/s3o-middleware-utils': {
+				publickey: {
+					poller: () => pollerStub,
+				},
+				authenticate: (publicKeyGetter) => {
+					publicKeyGetter.should.equal(pollerStub);
+
+					return {
+						authenticateToken: authenticateTokenStub,
+						validate: validatorStub,
+					}
+				}
 			},
 		});
+	});
+
+	afterEach(() => {
+		sandbox.restore();
+	});
+
+	it('exposes validate', () => {
+		s3o.validate.should.equal(validatorStub);
+	});
+
+	it('exposes a ready function which returns true when the poller resolves', () => {
+		pollerStub.withArgs({ promise: true }).returns(Promise.resolve());
+
+		return s3o.ready
+			.then((result) => {
+				return result.should.equal(true);
+			})
+			.catch(error => {
+				throw error;
+			})
 	});
 
 	runs.forEach((run) => {
 		describe('normal authentication', () => {
 			describe('when user POSTs a username', () => {
 				it('redirects if user authenticates', () => {
-					validatorStub.returns(true);
+					authenticateTokenStub.returns(true);
 					reqFixture.method = 'POST';
 					reqFixture.query = {
 						username: 'test',
@@ -95,7 +130,7 @@ describe('s3o-middleware', () => {
 
 					s3o(reqFixture, resFixture, nextStub);
 
-					validatorStub.withArgs('test', 'localhost', 'abc123').should.have.been.calledOnce;
+					authenticateTokenStub.withArgs('test', 'localhost', 'abc123').should.have.been.calledOnce;
 					resFixture.header.withArgs('Cache-Control', 'private, no-cache, no-store, must-revalidate')
 						.should.have.been.calledOnce;
 					resFixture.header.withArgs('Pragma', 'no-cache').should.have.been.calledOnce;
@@ -104,11 +139,11 @@ describe('s3o-middleware', () => {
 				});
 
 				it('responds with error message if authentication fails', () => {
-					validatorStub.returns(false);
+					authenticateTokenStub.returns(false);
 
 					s3o(reqFixture, resFixture, nextStub);
 
-					validatorStub.withArgs('test', 'localhost', 'test-test-123').should.have.been.calledOnce;
+					authenticateTokenStub.withArgs('test', 'localhost', 'test-test-123').should.have.been.calledOnce;
 					resFixture.send
 						.withArgs('<h1>Authentication error.</h1><p>For access, please log in with your FT account</p>')
 						.should.have.been.calledOnce;
@@ -118,21 +153,21 @@ describe('s3o-middleware', () => {
 			describe('when user has cookies', () => {
 				it('calls next() after authenticating cookies', () => {
 					reqFixture.method = 'GET';
-					validatorStub.returns(true);
+					authenticateTokenStub.returns(true);
 
 					s3o(reqFixture, resFixture, nextStub);
 
-					validatorStub.withArgs('test', 'localhost', 'test-test-123').should.have.been.calledOnce;
+					authenticateTokenStub.withArgs('test', 'localhost', 'test-test-123').should.have.been.calledOnce;
 					nextStub.should.have.been.calledOnce;
 				});
 
 				it('responds with error message if authentication fails', () => {
 					reqFixture.method = 'GET';
-					validatorStub.returns(false);
+					authenticateTokenStub.returns(false);
 
 					s3o(reqFixture, resFixture, nextStub);
 
-					validatorStub.withArgs('test', 'localhost', 'test-test-123').should.have.been.calledOnce;
+					authenticateTokenStub.withArgs('test', 'localhost', 'test-test-123').should.have.been.calledOnce;
 					nextStub.should.not.have.been.called;
 					resFixture.send.withArgs('<h1>Authentication error.</h1><p>For access, please log in with your FT account</p>')
 						.should.have.been.calledOnce;
@@ -165,24 +200,24 @@ describe('s3o-middleware', () => {
 
 		describe('no redirect authentication',() => {
 			it('calls next() on successful authentication',() => {
-				validatorStub.returns(true);
+				authenticateTokenStub.returns(true);
 
 				const result = s3o.authS3ONoRedirect(reqFixture,resFixture,nextStub);
 
-				validatorStub.withArgs('test','localhost','test-test-123').should.have.been.calledOnce;
+				authenticateTokenStub.withArgs('test','localhost','test-test-123').should.have.been.calledOnce;
 				nextStub.should.have.been.calledOnce;
 				result.should.equal('next returned');
 			});
 
 			it('returns 403 on authentication failure',() => {
-				validatorStub.returns(false);
+				authenticateTokenStub.returns(false);
 
 				s3o.authS3ONoRedirect(reqFixture,resFixture,nextStub);
 
-				validatorStub.withArgs('test','localhost','test-test-123').should.have.been.calledOnce;
+				authenticateTokenStub.withArgs('test','localhost','test-test-123').should.have.been.calledOnce;
 				nextStub.should.not.have.been.called;
-				resFixture.clearCookie.withArgs('s3o_username').should.have.been.calledOnce;
-				resFixture.clearCookie.withArgs('s3o_token').should.have.been.calledOnce;
+				resFixture.clearCookie.withArgs(cookies.USERNAME).should.have.been.calledOnce;
+				resFixture.clearCookie.withArgs(cookies.TOKEN).should.have.been.calledOnce;
 				resFixture.status.withArgs(403).should.have.been.calledOnce;
 				resFixture.send.withArgs('Forbidden').should.have.been.calledOnce;
 			});
